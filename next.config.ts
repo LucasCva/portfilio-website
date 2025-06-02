1dfa0f4af4176a100c48d79c5120c173de947662
LucasCva <lucasilva.cavalcant3@gmail.com>
2025-06-02 16:59:24 -0300

    Fix style

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: '/portfilio-website',
-  assetPrefix: '/portfilio-website/',
   reactStrictMode: true,
   images: {
     unoptimized: true
