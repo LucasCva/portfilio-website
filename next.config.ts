db98109d83cb707880c6a192e105b8d8f0ec7573
LucasCva <lucasilva.cavalcant3@gmail.com>
2025-06-02 16:48:55 -0300

    Fix style

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio/',
   reactStrictMode: true,
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
